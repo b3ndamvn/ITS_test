c9ccf36d26d6157ed4e5c3109d7e104547351b01
Nikita Benda <nikitabenda69@gmail.com>
2023-09-19 03:04:58 +0500

    change balance field type and create check_enough_balance method

--- moneypipe/models.py
+++ moneypipe/models.py
@@ -6,4 +6,9 @@ class MoneyUser(models.Model):
     name = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
     INN = models.CharField(max_length=12, unique=True)
-    balance = MoneyField(decimal_places=2, default_currency='RUB', max_digits=20)
+    balance = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def check_enough_balance(self, value):
+        if self.balance < float(value):
+            return False
+        return True
