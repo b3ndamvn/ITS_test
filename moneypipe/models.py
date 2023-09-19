d4dfc054b7b6c0b3e00a391ca89f01c8cdcbc79c
Nikita Benda <nikitabenda69@gmail.com>
2023-09-19 15:49:36 +0500

    add regexp for INN

--- moneypipe/models.py
+++ moneypipe/models.py
@@ -1,11 +1,17 @@
+from django.core.validators import RegexValidator
 from django.db import models
-from djmoney.models.fields import MoneyField
 
 
 class MoneyUser(models.Model):
     name = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    INN = models.CharField(max_length=12, unique=True)
+    INN = models.CharField(max_length=12, unique=True, validators=[
+        RegexValidator(
+            regex='^\d{12}$',
+            message='ИНН должно состоять из 12 цифр',
+            code='invalid_inn'
+        ),
+    ])
     balance = models.DecimalField(decimal_places=2, max_digits=20)
 
     def check_enough_balance(self, value):
